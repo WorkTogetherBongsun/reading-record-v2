140035eee86eb0dc1fa4b1e11bf2aabac9bab359
bongsun18 <cksgmlsp18@gmail.com>
2026-02-21 17:44:16 +0900

    Fix: Update Firebase Realtime Database URL to Asia-Southeast1 region

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,11 +10,11 @@ const firebaseConfig = {
   messagingSenderId: "257342307693",
   appId: "1:257342307693:web:bee3ae89a3274e66252e64",
   measurementId: "G-G5WY41RZ1Q",
-  databaseURL: "https://book-maker-ed6b4-default-rtdb.firebaseio.com/" // Realtime Database URL 추가
+  databaseURL: "https://book-maker-ed6b4-default-rtdb.asia-southeast1.firebasedatabase.app/" // 정확한 아시아 서버 URL 적용
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-const db = getDatabase(app); // Firestore 대신 Realtime Database 인스턴스 사용
+const db = getDatabase(app);
 const auth = getAuth(app);
 
 export { db, auth };
